997432616e9386205a496b8fe25a59beb794af27
Nikita <67905580+Nikita-Hritsay@users.noreply.github.com>
2021-02-21 13:20:50 +0200

    Update ConnectionMetaData.java

--- src/main/java/com/example/Practice6/DAO/ConnectionMetaData.java
+++ src/main/java/com/example/Practice6/DAO/ConnectionMetaData.java
@@ -4,5 +4,5 @@ public class ConnectionMetaData {
     public static final String Load_MySql_JDBC_Driver = "com.mysql.cj.jdbc.Driver";
     public static final String URL = "jdbc:mysql://localhost:3306/mydb?useUnicode=true&serverTimezone=UTC";
     public static final String USER = "root";
-    public static final String PASSWORD = "Fahatik12";
+    public static final String PASSWORD = "password";
 }
